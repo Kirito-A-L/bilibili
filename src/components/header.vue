45bcec0d4c6068e2517d1ccaa369aa52b86b0707
Kirito-A-L <694044061@qq.com>
2018-01-15 11:50:05 +0800

    更新

---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+     <div class="header">
+      
+     </div>
+</template>
+<script>
+export default {
+ name: "header",
+ data:function(){
+     return {data:"this is header"}
+ }
+}
+</script>
+<style>
+
+</style>
